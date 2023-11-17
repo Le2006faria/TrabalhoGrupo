64560a0569f0bed9ef15ca7f934591f7d768e304
Le2006faria <le2006faria@gmail.com>
2023-11-17 15:53:20 -0300

    .

---
+++ DOM/src/view/js/lagartixa.js
@@ -0,0 +1,3 @@
+function lagartixa(){
+    document.getElementById("lagartixa2").src="https://i.pinimg.com/originals/bd/f8/66/bdf86634deed4a573d040fb64252f1b7.gif";
+}
---
+++ DOM/src/view/js/pato.js
@@ -0,0 +1,3 @@
+function pato(){
+    document.getElementById("pato2").src="https://i.pinimg.com/originals/c5/bf/2f/c5bf2ff4f9c246287613527bfb189671.gif";
+}
